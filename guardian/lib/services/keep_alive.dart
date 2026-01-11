284d7d5fdf04522526c5746eeea9166bf21b281e
Yaya HC <hamedcuenca5@gmail.com>
2026-01-11 06:02:21 +0000

    chore: print current datetime before ping server

--- guardian/lib/services/keep_alive.dart
+++ guardian/lib/services/keep_alive.dart
@@ -6,6 +6,7 @@ import 'package:guardian/config/init.dart';
 class KeepAlive {
   static void run() {
     Timer.periodic(Duration(minutes: 10), (timer) async {
+      print('...${DateTime.now().toIso8601String()}');
       Client().get(Uri.parse(Init.dotEnv['SERVER_URL']));
     });
   }
